ccae03c22ddcdc7e3aee84bbd1bf15ad4b295df1
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-01-27 15:57:03 +0300

    WorkWithProperties

--- CS/Functions/Functions/OOP/WorkWithClasses.cs
+++ CS/Functions/Functions/OOP/WorkWithClasses.cs
@@ -4,23 +4,23 @@ namespace Functions.OOP
 {
     internal class WorkWithClasses
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
-            Player player1 = new Player("Nagibaotr", 2, "warior");
-            Player player2 = new Player("mega-lego", -3, "healler");
+            PlayerForInfo player1 = new PlayerForInfo("Nagibaotr", 2, "warior");
+            PlayerForInfo player2 = new PlayerForInfo("mega-lego", -3, "healler");
 
             player1.PrintInfo();
             player2.PrintInfo();
         }
     }
 
-    class Player
+    class PlayerForInfo
     {
         private string _nickName;
         private int _level = 0;
         private string _gameClass;
 
-        public Player(string nickName, int level, string gameClass)
+        public PlayerForInfo(string nickName, int level, string gameClass)
         {
             _nickName = nickName;
 
---
+++ CS/Functions/Functions/OOP/WorkWithProperties.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Functions.OOP
+{
+    internal class WorkWithProperties
+    {
+        static void Main1(string[] args)
+        {
+            PlayerForRender player1 = new PlayerForRender(20,25);
+            PlayerForRender player2 = new PlayerForRender(20,45);
+
+            Renderer render1 = new Renderer();
+            Renderer render2 = new Renderer('#');
+
+            render1.PrintPlayer(player1);
+            render2.PrintPlayer(player2);
+        }
+    }
+
+    class Renderer
+    {
+        private char _character = '&';
+
+        public Renderer(char character) 
+        {
+            this._character = character;
+        }
+
+        public Renderer() { }
+
+        public void PrintPlayer(PlayerForRender player)
+        {
+            Console.SetCursorPosition(player.XPosition, player.YPosition);
+            Console.WriteLine(_character);
+        }
+    }
+
+    class PlayerForRender
+    {
+        private int _xPosition;
+        private int _yPosition;
+        private int _xMaxPosition = 100;
+        private int _yMaxPosition = 150;
+
+        public PlayerForRender(int xPosition, int yPosition)
+        {
+            _xPosition = xPosition;
+            _yPosition = yPosition;
+        }
+
+        public int XPosition
+        {
+            get { return _xPosition; }
+            private set
+            {
+                if (value > 0 && value < _xMaxPosition)
+                {
+                    _xPosition = value;
+                }
+            }
+        }
+        public int YPosition
+        {
+            get { return _yPosition; }
+            private set
+            {
+                if (value > 0 && value < _yMaxPosition)
+                {
+                    _yPosition = value;
+                }
+            }
+        }
+    }
+}
