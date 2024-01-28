aaa386d9679a555f37db55c53bea9a3dfd3ef2ae
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-01-28 10:14:02 +0300

    WorkWithProperties Update

--- CS/Functions/Functions/OOP/WorkWithProperties.cs
+++ CS/Functions/Functions/OOP/WorkWithProperties.cs
@@ -4,7 +4,7 @@ namespace Functions.OOP
 {
     internal class WorkWithProperties
     {
-        static void Main1(string[] args)
+        static void Main(string[] args)
         {
             Player player1 = new Player(20,25,'@');
             Player player2 = new Player(200,45, '$');
@@ -30,11 +30,10 @@ namespace Functions.OOP
 
     class Player
     {
-        static private int s_xMaxPosition = 100;
-        static private int s_xMinPosition = 0;
-        static private int s_yMaxPosition = 150;
-        static private int s_yMinPosition = 0;
-        private char _character;
+        private static int s_xMaxPosition = 100;
+        private static int s_xMinPosition = 0;
+        private static int s_yMaxPosition = 150;
+        private static int s_yMinPosition = 0;
         private int _xPosition;
         private int _yPosition;
 
@@ -42,19 +41,19 @@ namespace Functions.OOP
         {
             _xPosition = SetValidPosition(xPosition, s_xMinPosition, s_xMaxPosition);
             _yPosition = SetValidPosition(yPosition, s_yMinPosition, s_yMaxPosition);
-            _character = character;
+            Character = character;
         }
 
-        public char Character { get { return _character; } }
-        public int XPosition
-        {
-            get { return _xPosition; }
-            private set {_xPosition = SetValidPosition(XPosition, s_xMinPosition, s_xMaxPosition); }
+        public char Character { get ; private set; } 
+        public int XPosition 
+        { 
+            get => _xPosition;
+            private set => _xPosition = value; 
         }
-        public int YPosition
-        {
-            get { return _yPosition; }
-            private set { _yPosition = SetValidPosition(YPosition, s_yMinPosition, s_yMaxPosition); }
+        public int YPosition 
+        { 
+            get => _yPosition; 
+            private set=> _xPosition = value; 
         }
 
         private int SetValidPosition(int position, int minValue, int maxValue)
