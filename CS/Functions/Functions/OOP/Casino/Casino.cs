9714864b3ed1ee9d41fd1c6e87aaa996744601c5
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-02-05 07:45:35 +0300

    Casino.cs update

--- CS/Functions/Functions/OOP/Casino/Casino.cs
+++ CS/Functions/Functions/OOP/Casino/Casino.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Functions.OOP.Casino
@@ -10,10 +8,89 @@ namespace Functions.OOP.Casino
     {
         static void Main()
         {
-
-
             DealerDeck dealerDeck = new DealerDeck();
-            dealerDeck.PrintInfo();
+
+            PlayerDeck playerDeck = new PlayerDeck(dealerDeck);
+
+            bool isUserExited = false;
+
+            while (isUserExited == false)
+            {
+                Console.Clear();
+
+                PrintUI();
+
+                int userInput = ReadInt();
+
+                switch (userInput)
+                {
+                    case (int)UserCommands.TakeOneCard:
+                        playerDeck.TakeOneCard();
+                        break;
+                    case (int)UserCommands.TakeSeveralCards:
+                        TakeSeveralCardsUI(playerDeck);
+                        break;
+                    case (int)UserCommands.PrintMyCards:
+                        playerDeck.PrintInfo();
+                        break;
+                    case (int)UserCommands.Exit:
+                        isUserExited = true;
+                        break;
+                    default:
+                        Console.WriteLine("You entered the wrong command. Try again, please.");
+                        break;
+                }
+
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+            }
+
+        }
+
+        static void PrintUI()
+        {
+            Console.WriteLine("Enter command:\n" +
+                $"{(int)UserCommands.TakeOneCard} - if you need one more card\n" +
+                $"{(int)UserCommands.TakeSeveralCards} - if you need to take several cards\n" +
+                $"{(int)UserCommands.PrintMyCards} - print your cards info\n" +
+                $"{(int)UserCommands.Exit} - exit\n");
+        }
+
+        static int ReadInt()
+        {
+            bool isIntEntered = false;
+            int parsedInt = 0;
+
+            while (isIntEntered == false)
+            {
+                string enteredString;
+
+                Console.WriteLine("Enter integer:");
+                enteredString = Console.ReadLine();
+
+                isIntEntered = int.TryParse(enteredString, out parsedInt);
+
+                if (isIntEntered)
+                {
+                    Console.WriteLine($"The entered number is recognized, it is: {parsedInt}");
+                }
+                else
+                {
+                    Console.WriteLine("You entered the wrong number. Try again, please.\n");
+                }
+            }
+
+            return parsedInt;
+        }
+
+        static void TakeSeveralCardsUI(PlayerDeck playerDeck)
+        {
+            int cardsCount;
+
+            Console.WriteLine("Enter the number of cards you need");
+
+            cardsCount = ReadInt();
+            playerDeck.TakeSeveralCards(cardsCount);
         }
     }
 
@@ -21,14 +98,14 @@ namespace Functions.OOP.Casino
     {
         protected List<Card> cards;
 
-        protected DeckOfCards() 
+        protected DeckOfCards()
         {
             cards = new List<Card>();
         }
 
         public void PrintInfo()
         {
-            foreach(Card card in cards)
+            foreach (Card card in cards)
             {
                 card.PrintInfo();
             }
@@ -37,7 +114,7 @@ namespace Functions.OOP.Casino
 
     class DealerDeck : DeckOfCards
     {
-        public DealerDeck() 
+        public DealerDeck()
         {
             foreach (Values value in Enum.GetValues(typeof(Values)))
             {
@@ -51,26 +128,82 @@ namespace Functions.OOP.Casino
 
         public Card DealCard()
         {
-            Card card = null;
-            return card;
+            if (cards.Count > 0)
+            {
+                Card card;
+                Random random = new Random();
+                int cardsInDeck = cards.Count;
+                int cardIndex = random.Next(0, cardsInDeck);
+
+                card = cards[cardIndex];
+                cards.RemoveAt(cardIndex);
+                return card;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 
     class PlayerDeck : DeckOfCards
     {
-        DealerDeck _dealer;
+        private DealerDeck _dealerDeck;
 
-        public PlayerDeck(DealerDeck dealer) 
+        public PlayerDeck(DealerDeck dealer)
         {
-            _dealer = dealer;
+            if (_dealerDeck != null)
+            {
+                _dealerDeck = dealer;
+            }
+            else
+            {
+                Console.WriteLine("It looks like you have no dealer =(");
+            } 
         }
 
-        public void GetDealerCard()
+        public void TakeOneCard()
         {
-            cards.Add(_dealer.DealCard());
+            Card nextCard = null;
+
+            if (_dealerDeck != null)
+            {
+                nextCard = _dealerDeck.DealCard();
+            }
+            else
+            {
+                Console.WriteLine("It looks like you have no more dealer =(");
+            }
+
+            if (nextCard != null)
+            {
+                cards.Add(nextCard);
+
+                Console.WriteLine("You took card:");
+                nextCard.PrintInfo();
+            }
+            else
+            {
+                Console.WriteLine("You couldn't take a card. It looks like the dealer has run out of them =(");
+            }
         }
 
-        public void 
+        public void TakeSeveralCards(int cardsCount)
+        {
+            if (cardsCount >= 1)
+            {
+                for (int i = 0; i < cardsCount; i++)
+                {
+                    TakeOneCard();
+                    Console.WriteLine("Wait a second");
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+            else
+            {
+                Console.WriteLine("It is necessary to enter a positive integer number of cards.");
+            }
+        }
     }
 
     class Card
@@ -113,5 +246,13 @@ namespace Functions.OOP.Casino
         Jack = 11,
         Queen = 12,
         King = 13
+    }
+
+    enum UserCommands
+    {
+        TakeOneCard = 1,
+        TakeSeveralCards = 2,
+        PrintMyCards = 3,
+        Exit = 4,
     }
 }
