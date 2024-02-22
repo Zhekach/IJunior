95806c157260fd69f1f8d82abbb5fff8d5dafdf8
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-02-22 10:13:08 +0300

    Train Start

---
+++ CS/Functions/Functions/OOP/TrainProgram/TrainProgram.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions.OOP.TrainProgram
+{
+    internal class TrainProgram
+    {
+    }
+
+    class Train
+    {
+        private int _passengersCount = 0;
+        private List<TrainCar> _cars = new List<TrainCar>();
+        private bool _isLeft = false;
+        public Route Route { get; private set; }
+
+    }
+
+    class TrainCar
+    {
+        private Dictionary<string, int> _capacity = new Dictionary<string, int>() 
+        {
+            {"Сидячий", 107},
+            {"Общий", 72},
+            {"Плацкарт", 54},
+            {"Купе", 36},
+            {"СВ", 20},
+            {"Люкс", 8},
+        };
+    }
+
+    struct Route
+    {
+        public string Departure {  get; private set; }
+        public string Arrival {  get; private set; }
+
+        public Route(string departure, string arrival)
+        {
+            Departure = departure;
+            Arrival = arrival;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Место отправдения - {Departure}, есто прибытия - {Arrival}.");
+        }
+    }
+}
