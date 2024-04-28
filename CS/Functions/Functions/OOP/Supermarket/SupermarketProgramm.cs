153d4356f6cdf1f513e007fcfc8a64e50e94e87c
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-04-28 22:43:00 +0300

    InProgress

--- CS/Functions/Functions/OOP/Supermarket/SupermarketProgramm.cs
+++ CS/Functions/Functions/OOP/Supermarket/SupermarketProgramm.cs
@@ -12,10 +12,10 @@ namespace Functions.OOP.Supermarket
             
             List<Client> clients = new List<Client>()
             {
-                new Client(supermarket, 5, 100 ),
+                new Client(supermarket, 5, 100),
                 new Client(supermarket, 3, 200),
-                //new Client(supermarket, 100),
-                //new Client(supermarket, 100)
+                //new Client(supermarket, 10, 100),
+                //new Client(supermarket, 4, 100)
             };
             
             supermarket.Run(clients);
@@ -37,28 +37,23 @@ namespace Functions.OOP.Supermarket
 
         public void Run(List<Client> clients)
         {
+            Console.WriteLine("Супермаркет начинает свою работу");
+
             foreach (Client client in clients)
             {
-                bool hasClientPaid = false;
+                Console.WriteLine("\nНовый покупатель выбирает покупки");
+
                 client.ChoseProducts();
                 
-                while (hasClientPaid == false)
+                while (client.IsBasketPaid == false)
                 {
                     float totalPrice = GetTotalPrice(client);
                     
                     if (client.PayProducts(totalPrice))
                     {
                         _revenue += totalPrice;
-                        hasClientPaid = true;
-
-                        Console.WriteLine("Клиент оплатил покупки");
+                        client.PackProducts();
                     }
-                    else
-                    {
-                        
-                        Console.WriteLine("Клиент не смог оплатить покупки");
-                    }
-                    //TODO вариант, если у клиента всё равно недостаточно денег!!!
                 }
             }
 
@@ -85,16 +80,20 @@ namespace Functions.OOP.Supermarket
         private int _maxProductsCount;
         private float _money;
         private Supermarket _supermarket;
+        private Bag _bag;
         
         public Client(Supermarket supermarket, int maxProductsCount, int money)
         {
             _maxProductsCount = maxProductsCount;
             _money = money;
             _supermarket = supermarket;
+            _bag = new Bag();
             Basket = new Basket();
+            IsBasketPaid = false;
         }
 
         public Basket Basket { get; private set; }
+        public bool IsBasketPaid { get; private set; }
 
         public void ChoseProducts()
         {
@@ -106,22 +105,36 @@ namespace Functions.OOP.Supermarket
             if (totalPrice <= _money)
             {
                 _money -= totalPrice;
+                IsBasketPaid = true;
+
+                Console.WriteLine("Клиент оплатил покупки");
+
                 return true;
             }
             else
             {
+                Console.WriteLine("Клиент не смог оплатить покупки");
+                Console.Write("Клиент выкладывает один продукт: ");
+                
                 Basket.RemoveRandomProduct();
 
-                Console.Write("Клиент выложил один продукт: ");
-                
                 return false;
             }
+        }
+
+        public void PackProducts()
+        {
+            Console.WriteLine("Клиент упаковывает покупки");
+            
+            _bag.AddProducts(Basket.RemoveAllProducts());
+
+            Console.WriteLine("Клиент упаковал покупки и ушёл довольный");
         }
     }
 
     internal class Basket
     {
-        private readonly Random _random = new Random();
+        private Random _random = new Random();
         public List<Product> Products;
 
         public Basket()
@@ -148,6 +161,20 @@ namespace Functions.OOP.Supermarket
             Products.RemoveAt(removeIndex);
         }
 
+        public List<Product> RemoveAllProducts()
+        {
+            List<Product> copyOfProducts = new List<Product>();
+
+            foreach (Product product in Products)
+            {
+                copyOfProducts.Add(product);
+            }
+
+            Products.RemoveAll(Predicate<Product>);
+
+            return copyOfProducts;
+        }
+
         public void AddRandomProducts(Supermarket supermarket, int maxCount)
         {
             int productsCount = _random.Next(1, maxCount + 1);
@@ -160,6 +187,19 @@ namespace Functions.OOP.Supermarket
                 Products.Add(supermarket.Products[newProductId]);
                 
                 Console.WriteLine($"В корзину добавлен новый продукт: {supermarket.Products[newProductId].Name}");
+            }
+        }
+    }
+
+    internal class Bag
+    {
+        public List<Product> Products;
+
+        public void AddProducts(List<Product> newProducts)
+        {
+            foreach (Product product in newProducts)
+            {
+                Products.Add(product);
             }
         }
     }
