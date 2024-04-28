2781f7e7e550a5ee706a652096ef744d6ef77ccd
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-04-29 02:52:42 +0300

    War add abstract soldier and its fields

---
+++ CS/Functions/Functions/OOP/War/WarProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Functions.OOP.War
+{
+    internal class WarProgram
+    {
+    }
+
+    internal abstract class Soldier
+    {
+        protected readonly float BaseHealth = 50;
+        protected readonly float BasePower = 15;
+        protected readonly float BaseArmor = 10;
+
+        protected readonly int UpdatingPoints = 9;
+
+        protected Soldier() 
+        {
+            Health = BaseHealth; 
+            Power = BasePower; 
+            Armor = BaseArmor;
+        }
+
+        public float Health {  get; private set; }
+        public float Power {  get; private set; }
+        public float Armor {  get; private set; }
+    }
+}
