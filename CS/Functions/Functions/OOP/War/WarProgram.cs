917b95374d20e5b23478d0db234cac5e29c01170
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-06-25 17:31:10 +0300

    War add random stats distribution

--- CS/Functions/Functions/OOP/War/WarProgram.cs
+++ CS/Functions/Functions/OOP/War/WarProgram.cs
@@ -6,25 +6,79 @@ namespace Functions.OOP.War
 {
     internal class WarProgram
     {
+        public static void Main()
+        {
+            Soldier testSoldier = new Soldier();
+            testSoldier.PrintInfo();
+        }
     }
 
-    internal abstract class Soldier
+    internal class Soldier
     {
-        protected readonly float BaseHealth = 50;
-        protected readonly float BasePower = 15;
-        protected readonly float BaseArmor = 10;
+        static Random s_random = new Random();
 
-        protected readonly int UpdatingPoints = 9;
-
-        protected Soldier() 
+        private readonly Dictionary<Enum, float> _stats = new Dictionary<Enum, float>() 
         {
-            Health = BaseHealth; 
-            Power = BasePower; 
-            Armor = BaseArmor;
+            { SoldierSats.Health, 0 },
+            { SoldierSats.Power, 0 },
+            { SoldierSats.Armor, 0 }
+        };
+        private readonly Dictionary<Enum, float> _statsIncrements = new Dictionary<Enum, float>()
+        {
+            { SoldierSats.Health, 1f },
+            { SoldierSats.Power, 0.75f },
+            { SoldierSats.Armor, 0.5f }
+        };
+        private int StatsPoints = 9;
+
+        //protected Soldier() 
+        public Soldier() 
+        {
+            this.DistributeStats();
+            Health = _stats[SoldierSats.Health]; 
+            Power = _stats[SoldierSats.Power];
+            Armor = _stats[SoldierSats.Armor];
         }
 
         public float Health {  get; private set; }
         public float Power {  get; private set; }
         public float Armor {  get; private set; }
+
+        private void DistributeStats()
+        {
+            while (StatsPoints > 0)
+            {
+                List<Enum> stats = new List<Enum>(_stats.Keys);
+                
+                Enum currentStat = stats[s_random.Next(stats.Count)];
+
+                float currentStatValue = _stats[currentStat];
+                float currentStatIncrement = _statsIncrements[currentStat];
+
+                if(_stats.TryGetValue(currentStat, out currentStatValue))
+                {
+                    _stats.Remove(currentStat);
+                    _stats.Add(currentStat, currentStatValue += currentStatIncrement);
+                    StatsPoints--;
+                }
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Тут должен быть тип солдата");
+
+            foreach(var stat in _stats)
+            {
+                Console.WriteLine($"Параметр: {stat.Key} Значение: {stat.Value}");
+            }
+        }
     }
+
+    enum SoldierSats
+        {
+            Health = 0, 
+            Power = 1,
+            Armor = 2
+        }
 }
