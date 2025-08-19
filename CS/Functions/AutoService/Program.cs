3a7b98b6f1650f80cc53ce3f03e6a0f1782734d1
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-08-19 17:11:29 +0300

    refactor mechanic DetailRepair

--- CS/Functions/AutoService/Program.cs
+++ CS/Functions/AutoService/Program.cs
@@ -14,14 +14,15 @@ internal class AutoService
     private List<Detail> _details;
     private int _balance;
 
+    private readonly Mechanic _mechanic;
     private readonly Appraiser _appraiser = new Appraiser();
-    private readonly Mechanic _mechanic = new Mechanic();
 
     public AutoService(Queue<Car> cars, List<Detail> details, int balance)
     {
         _cars = cars;
         _details = details;
         _balance = balance;
+        _mechanic = new Mechanic(details);
     }
 
     public void PrintInfo()
@@ -73,6 +74,13 @@ internal class Appraiser
 
 internal class Mechanic
 {
+    private readonly List<Detail> _detailsInStock;
+
+    public Mechanic(List<Detail> detailsInStock)
+    {
+        _detailsInStock = detailsInStock;
+    }
+
     public bool CheckIsCarRepaired(Car car)
     {
         bool isCarRepaired = false;
@@ -81,42 +89,28 @@ internal class Mechanic
         {
             if (detail.IsBroken)
             {
-                return false;
+                return isCarRepaired;
             }
         }
 
-        return true;
+        isCarRepaired = true;
+
+        return isCarRepaired;
     }
 
-    public bool TryRepairDetail(Car car, List<Detail> detailsInStock)
+    public bool TryRepairDetail(Car car, Detail brokenDetail)
     {
         bool isDetailRepaired = false;
-        List<Detail> carDetails = new List<Detail>(car.Details);
-
-        foreach (var detail in carDetails)
+        Detail? newDetail = FindDetailToRepair(brokenDetail, _detailsInStock);
+        
+        if (newDetail == null ||
+            car.TryRepairDetail(newDetail) == false)
         {
-            if (detail.IsBroken == false)
-            {
-                continue;
-            }
-            
-            Detail? detailToRepair = FindDetailToRepair(detail, detailsInStock);
-
-            if (detailToRepair == null)
-            {
-                continue;
-            }
-
-            if (car.TryRepairDetail(detailToRepair) == false)
-            {
-                continue;
-            }
-            
-            detailsInStock.Remove(detailToRepair);
-            isDetailRepaired = true;
-            
-            break;
+            return isDetailRepaired;
         }
+
+        _detailsInStock.Remove(brokenDetail);
+        isDetailRepaired = true;
 
         return isDetailRepaired;
     }
@@ -133,266 +127,266 @@ internal class Mechanic
                 break;
             }
         }
-        
+
         return detailToRepair;
     }
 }
 
-    internal class AutoServiceFactory
+internal class AutoServiceFactory
+{
+    private const int minDetailsCount = 10;
+    private const int maxDetailsCount = 20;
+    private const int minBalance = 100;
+    private const int maxBalance = 5000;
+    private const int minCarsCount = 2;
+    private const int maxCarsCount = 4;
+
+    private DetailFactory _detailFactory = new DetailFactory();
+    private CarFactory _carFactory = new CarFactory();
+
+    public AutoService GenerateAutoService()
     {
-        private const int minDetailsCount = 10;
-        private const int maxDetailsCount = 20;
-        private const int minBalance = 100;
-        private const int maxBalance = 5000;
-        private const int minCarsCount = 2;
-        private const int maxCarsCount = 4;
+        int detailsCount = Utility.GetRandomInt(minDetailsCount, maxDetailsCount);
+        int balance = Utility.GetRandomInt(minBalance, maxBalance);
+        int carsCount = Utility.GetRandomInt(minCarsCount, maxCarsCount);
 
-        private DetailFactory _detailFactory = new DetailFactory();
-        private CarFactory _carFactory = new CarFactory();
+        List<Detail> details = _detailFactory.GenerateRandomDetails(detailsCount);
+        List<Car> cars = _carFactory.GenerateCars(carsCount);
 
-        public AutoService GenerateAutoService()
-        {
-            int detailsCount = Utility.GetRandomInt(minDetailsCount, maxDetailsCount);
-            int balance = Utility.GetRandomInt(minBalance, maxBalance);
-            int carsCount = Utility.GetRandomInt(minCarsCount, maxCarsCount);
+        AutoService autoService = new AutoService(new Queue<Car>(cars), details, balance);
 
-            List<Detail> details = _detailFactory.GenerateRandomDetails(detailsCount);
-            List<Car> cars = _carFactory.GenerateCars(carsCount);
+        return autoService;
+    }
+}
 
-            AutoService autoService = new AutoService(new Queue<Car>(cars), details, balance);
+class Car
+{
+    private HashSet<Detail> _details;
 
-            return autoService;
-        }
+    public Car(HashSet<Detail> details)
+    {
+        _details = new HashSet<Detail>(details);
     }
 
-    class Car
+    public IReadOnlyCollection<Detail> Details => _details;
+
+    public bool TryRepairDetail(Detail newDetail)
     {
-        private HashSet<Detail> _details;
+        bool isRepaired = false;
 
-        public Car(HashSet<Detail> details)
+        if (newDetail.IsBroken)
         {
-            _details = new HashSet<Detail>(details);
+            return false;
         }
 
-        public IReadOnlyCollection<Detail> Details => _details;
+        Detail brokenDetail = null;
 
-        public bool TryRepairDetail(Detail detailToRepair)
+        foreach (var detail in _details)
         {
-            bool isRepaired = false;
-
-            if (detailToRepair.IsBroken)
+            if (detail.Type == newDetail.Type)
             {
-                return false;
+                brokenDetail = detail;
+                break;
             }
-
-            Detail brokenDetail = null;
-
-            foreach (var detail in _details)
-            {
-                if (detail.Type == detailToRepair.Type)
-                {
-                    brokenDetail = detail;
-                    break;
-                }
-            }
-
-            if (brokenDetail == null)
-            {
-                return false;
-            }
-
-            _details.Remove(brokenDetail);
-            _details.Add(detailToRepair);
-            isRepaired = true;
-
-            return isRepaired;
         }
 
-        public void PrintInfo()
+        if (brokenDetail == null)
         {
-            Console.WriteLine($"Информация об автомобиле, сломаны детали:");
-
-            foreach (var detail in _details)
-            {
-                if (detail.IsBroken)
-                {
-                    detail.PrintInfo();
-                }
-            }
+            return false;
         }
+
+        _details.Remove(brokenDetail);
+        _details.Add(newDetail);
+        isRepaired = true;
+
+        return isRepaired;
     }
 
-    class CarFactory
+    public void PrintInfo()
     {
-        private const int MinBrokenDetails = 1;
+        Console.WriteLine($"Информация об автомобиле, сломаны детали:");
 
-        private DetailFactory _detailFactory = new DetailFactory();
-
-        public List<Car> GenerateCars(int count)
+        foreach (var detail in _details)
         {
-            List<Car> cars = new List<Car>();
-
-            for (int i = 0; i < count; i++)
+            if (detail.IsBroken)
             {
-                Car car = GenerateCar();
-                BrokeRandomDetails(car);
-                cars.Add(car);
-            }
-
-            return cars;
-        }
-
-        private Car GenerateCar()
-        {
-            HashSet<Detail> details = new HashSet<Detail>();
-
-            List<DetailType> detailTypes = new List<DetailType>(
-                (DetailType[])Enum.GetValues(typeof(DetailType)));
-
-            foreach (var detailType in detailTypes)
-            {
-                Detail newDetail = _detailFactory.GenerateDetail(detailType);
-                details.Add(newDetail);
-            }
-
-            Car car = new Car(details);
-
-            return car;
-        }
-
-        private void BrokeRandomDetails(Car car)
-        {
-            int brokenDetailsCount = Utility.GetRandomInt(MinBrokenDetails, car.Details.Count);
-
-            for (int i = 0; i < brokenDetailsCount; i++)
-            {
-                int index = Utility.GetRandomInt(0, car.Details.Count - 1);
-                Detail detailToBroke = car.Details.ElementAt(index);
-                detailToBroke.SetBroken(true);
+                detail.PrintInfo();
             }
         }
     }
+}
 
-    class Detail
+class CarFactory
+{
+    private const int MinBrokenDetails = 1;
+
+    private DetailFactory _detailFactory = new DetailFactory();
+
+    public List<Car> GenerateCars(int count)
     {
-        public Detail(DetailType type, int price)
+        List<Car> cars = new List<Car>();
+
+        for (int i = 0; i < count; i++)
         {
-            Type = type;
-            Price = price;
+            Car car = GenerateCar();
+            BrokeRandomDetails(car);
+            cars.Add(car);
         }
 
-        public DetailType Type { get; }
-        public int Price { get; }
-        public bool IsBroken { get; private set; } = false;
-
-        public void SetBroken(bool isBroken) => IsBroken = isBroken;
-
-        public void PrintInfo()
-        {
-            Console.WriteLine($"Тип детали - {Type}, стоимость - {Price} рублей." +
-                              $"Состояние: сломана - {IsBroken}");
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (obj is not Detail other) return false;
-            return Type == other.Type && Price == other.Price;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Type, Price);
-        }
+        return cars;
     }
 
-    internal class DetailFactory
+    private Car GenerateCar()
     {
-        private const int MaxPrice = 1000;
-        private const int MinPrice = 100;
+        HashSet<Detail> details = new HashSet<Detail>();
 
-        public List<Detail> GenerateRandomDetails(int count)
+        List<DetailType> detailTypes = new List<DetailType>(
+            (DetailType[])Enum.GetValues(typeof(DetailType)));
+
+        foreach (var detailType in detailTypes)
         {
-            List<Detail> details = new List<Detail>();
+            Detail newDetail = _detailFactory.GenerateDetail(detailType);
+            details.Add(newDetail);
+        }
 
-            for (int i = 0; i < count; i++)
+        Car car = new Car(details);
+
+        return car;
+    }
+
+    private void BrokeRandomDetails(Car car)
+    {
+        int brokenDetailsCount = Utility.GetRandomInt(MinBrokenDetails, car.Details.Count);
+
+        for (int i = 0; i < brokenDetailsCount; i++)
+        {
+            int index = Utility.GetRandomInt(0, car.Details.Count - 1);
+            Detail detailToBroke = car.Details.ElementAt(index);
+            detailToBroke.SetBroken(true);
+        }
+    }
+}
+
+class Detail
+{
+    public Detail(DetailType type, int price)
+    {
+        Type = type;
+        Price = price;
+    }
+
+    public DetailType Type { get; }
+    public int Price { get; }
+    public bool IsBroken { get; private set; } = false;
+
+    public void SetBroken(bool isBroken) => IsBroken = isBroken;
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Тип детали - {Type}, стоимость - {Price} рублей." +
+                          $"Состояние: сломана - {IsBroken}");
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Detail other) return false;
+        return Type == other.Type && Price == other.Price;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Type, Price);
+    }
+}
+
+internal class DetailFactory
+{
+    private const int MaxPrice = 1000;
+    private const int MinPrice = 100;
+
+    public List<Detail> GenerateRandomDetails(int count)
+    {
+        List<Detail> details = new List<Detail>();
+
+        for (int i = 0; i < count; i++)
+        {
+            DetailType type = Utility.GetRandomEnumValue<DetailType>();
+            Detail detail = GenerateDetail(type);
+            details.Add(detail);
+        }
+
+        return details;
+    }
+
+    public Detail GenerateDetail(DetailType type)
+    {
+        int price = Utility.GetRandomInt(MinPrice, MaxPrice);
+
+        Detail detail = new Detail(type, price);
+
+        return detail;
+    }
+}
+
+enum DetailType
+{
+    Wheel,
+    Engine,
+    Body,
+    Chassis,
+    Brake,
+    Gearbox,
+    Suspension,
+    Lights
+}
+
+internal static class Utility
+{
+    private static readonly Random s_random = new Random();
+
+    public static int GetRandomInt(int maxValue)
+    {
+        return s_random.Next(maxValue);
+    }
+
+    public static int GetRandomInt(int minValue, int maxValue)
+    {
+        return s_random.Next(minValue, maxValue);
+    }
+
+    public static int ReadInt()
+    {
+        bool isIntEntered = false;
+        int parsedInt = 0;
+
+        while (isIntEntered == false)
+        {
+            string enteredString;
+
+            Console.WriteLine("Введите целое число:");
+            enteredString = Console.ReadLine();
+
+            isIntEntered = int.TryParse(enteredString, out parsedInt);
+
+            if (isIntEntered)
             {
-                DetailType type = Utility.GetRandomEnumValue<DetailType>();
-                Detail detail = GenerateDetail(type);
-                details.Add(detail);
+                Console.WriteLine($"Введенное число распознанно, это: {parsedInt}");
             }
-
-            return details;
-        }
-
-        public Detail GenerateDetail(DetailType type)
-        {
-            int price = Utility.GetRandomInt(MinPrice, MaxPrice);
-
-            Detail detail = new Detail(type, price);
-
-            return detail;
-        }
-    }
-
-    enum DetailType
-    {
-        Wheel,
-        Engine,
-        Body,
-        Chassis,
-        Brake,
-        Gearbox,
-        Suspension,
-        Lights
-    }
-
-    internal static class Utility
-    {
-        private static readonly Random s_random = new Random();
-
-        public static int GetRandomInt(int maxValue)
-        {
-            return s_random.Next(maxValue);
-        }
-
-        public static int GetRandomInt(int minValue, int maxValue)
-        {
-            return s_random.Next(minValue, maxValue);
-        }
-
-        public static int ReadInt()
-        {
-            bool isIntEntered = false;
-            int parsedInt = 0;
-
-            while (isIntEntered == false)
+            else
             {
-                string enteredString;
-
-                Console.WriteLine("Введите целое число:");
-                enteredString = Console.ReadLine();
-
-                isIntEntered = int.TryParse(enteredString, out parsedInt);
-
-                if (isIntEntered)
-                {
-                    Console.WriteLine($"Введенное число распознанно, это: {parsedInt}");
-                }
-                else
-                {
-                    Console.WriteLine("Вы ввели неверно, попробуйте ещё раз)\n");
-                }
+                Console.WriteLine("Вы ввели неверно, попробуйте ещё раз)\n");
             }
-
-            return parsedInt;
         }
 
-        public static T GetRandomEnumValue<T>() where T : Enum
-        {
-            Array values = Enum.GetValues(typeof(T));
-            int index = GetRandomInt(values.Length);
-            var result = (T)values.GetValue(index)!;
-
-            return result;
-        }
+        return parsedInt;
     }
+
+    public static T GetRandomEnumValue<T>() where T : Enum
+    {
+        Array values = Enum.GetValues(typeof(T));
+        int index = GetRandomInt(values.Length);
+        var result = (T)values.GetValue(index)!;
+
+        return result;
+    }
+}
