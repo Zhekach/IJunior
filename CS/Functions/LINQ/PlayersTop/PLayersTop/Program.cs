37b5c611b193adc83c096cdf0ca5bab7dbb302e1
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-09-03 18:34:04 +0300

    add PlayersTop base file

---
+++ CS/Functions/LINQ/PlayersTop/PLayersTop/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    
+}
+
+internal class Player
+{
+    public Player(string name, int level, int power)
+    {
+        Name = name;
+        Level = level;
+        Power = power;
+    }
+    
+    public string Name { get; private set; }
+    public int Level { get; private set; }
+    public int Power { get; private set; }
+}
